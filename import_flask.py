efb2a7b33f76efb0575d26995bc6dc87e289dc8c
Gerry <gerrysabar@gmail.com>
2019-07-06 13:53:18 +0700

    SQLAlchemy to Swagger

--- import_flask.py
+++ import_flask.py
@@ -1,19 +1,47 @@
-from flask import Flask, request
-from flask_restplus import Api, Resource
-from log import Log
+from flask import Flask, request, jsonify
+from flask_restplus import Api, Resource, reqparse, fields
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow import Schema, fields, pprint
 
-flask_app = Flask(__name__)
-app = Api(app=flask_app)
+# Customisasi Flask
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/my_project'
 
-name_space = app.namespace('main', description='Main APIs')
-sql = SQLAlchemy()
+# Flask Restplus
+api = Api(app)
+name_space = api.namespace('main', description='Main APIs')
+
+# SQLAlchemy
+db = SQLAlchemy(app)
+
+my_fields = api.model('MyModel', {
+    'detail': fields.String,
+})
+
+class LogSchema(Schema):
+    id = fields.Int()
+    detail = fields.Str()
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    detail = db.Column(db.Text)
+
 
 @name_space.route("/<int:id>")
-class Log(Resource):
-    @app.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'},
-             params={'id': 'Specify the Id associated with the person'})
-    def get(self,id):
+class LogDetail(Resource):
+    @api.doc(
+        responses=
+        {
+            200: 'OK',
+            400: 'Invalid Argument',
+            500: 'Mapping Key Error'
+        },
+        params=
+        {
+            'id': 'Specify the Id associated with the person'
+        })
+    def get(self, id):
         return {
             "status": "Got new data " + str(id)
         }
@@ -31,20 +59,41 @@ class Log(Resource):
 
 @name_space.route("/")
 class LogList(Resource):
+    @api.doc(
+        responses=
+        {
+            200: 'OK',
+            400: 'Invalid Argument',
+            500: 'Mapping Key Error'
+        },
+        params=
+        {
+            'detail': 'Log detail text'
+        })
     def get(self):
-
-        log = Log()
-        log.detail = 'xxxx'
-        sql.session.add(log)
-        sql.session.commit()
-
+        logs = Log.query.all()
+        result = []
+        resultSchema = []
+        schema = LogSchema()
+        for log in logs:
+            result.append({
+                'id': log.id,
+                'detail': log.detail,
+            })
+            resultSchema.append(schema.dump(log))
         return {
-            "status": "List all data"
+            "data": result,
+            "dataSchema": resultSchema
         }
 
-    @app.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'},
-             params={'detail': 'Log detail text'})
+    @api.doc(model='MyModel', body=my_fields)
     def post(self):
+        payload = request.get_json()
+        log = Log()
+        log.detail = payload['detail']
+        db.session.add(log)
+        db.session.commit()
+
         return {
-            "status": "Post new data"
+            "data": log
         }
